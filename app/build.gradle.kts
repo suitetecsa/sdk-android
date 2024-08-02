e91524102f31e8ce98b779481979ee681f80edb1
lesclaz <lesclaz95@gmail.com>
2024-08-02 02:21:25 -0400

    set nimSdk to 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "cu.suitetecsa.sdkandroid"
-        minSdk = 22
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
